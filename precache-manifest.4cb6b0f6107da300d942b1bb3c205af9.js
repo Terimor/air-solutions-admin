f4733f2e21425ab885ebabdd597bde616fe99fe5
Vlad Kyrychenko <vlad.kyr199@gmail.com>
2020-09-22 23:42:45 +0300

    init commit

---
+++ precache-manifest.4cb6b0f6107da300d942b1bb3c205af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a38bad2d58f1499bfc8908a34c6134",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de10f1aecdc76a92c0f0",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "0a9d69ca4bc58c5deccb",
+    "url": "/static/css/main.18b5d709.chunk.css"
+  },
+  {
+    "revision": "de10f1aecdc76a92c0f0",
+    "url": "/static/js/2.6dcf2ceb.chunk.js"
+  },
+  {
+    "revision": "f606f9887f2636d6532333256d69f990",
+    "url": "/static/js/2.6dcf2ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a9d69ca4bc58c5deccb",
+    "url": "/static/js/main.6b8b1ca7.chunk.js"
+  },
+  {
+    "revision": "11ae51cb9849cbe54a56",
+    "url": "/static/js/runtime-main.ff4b4397.js"
+  }
+]);
